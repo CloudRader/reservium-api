a7be849fc464dd7ef026d87e42921b22e4bf3202
Artem Kuznetsov <artyom.20century@gmail.com>
2024-02-29 18:04:39 +0100

    ✅[Add] "Authorisation users from IS"

---
+++ app/app/api/buk_is_auth.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, Depends, APIRouter, Query
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from fastapi.responses import RedirectResponse
+
+import httpx
+
+app = FastAPI()
+
+router = APIRouter(
+    prefix='/auth_is'
+)
+
+client_id = "e36219f8fdd7619dfa80754aa17c47e38c04e4407d37c26e48058531c82b18c1"
+client_secret = "44218c6184e21ee1e679586bcbe8d5b1727d6a771c6619a46aa821fe1eff4e98"
+redirect_uri = "https://10.0.52.106:8000/auth_is/login/callback"
+
+# OAuth 2.0 Authorization Code flow configuration
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl=f"https://is.buk.cvut.cz/oauth/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}",
+    tokenUrl="https://is.buk.cvut.cz/oauth/token",
+    auto_error=False,
+)
+
+
+@router.get("/login")
+async def login():
+    # Manually construct the authorization URL
+    authorization_url = (
+        f"https://is.buk.cvut.cz/oauth/authorize?client_id={client_id}"
+        "&response_type=code&scope=location"  # Include the "location" scope
+        f"&redirect_uri={redirect_uri}"
+    )
+
+    return RedirectResponse(url=authorization_url)
+
+
+@router.get("/")
+async def read_root():
+    return {"message": "Hello, OAuth 2.0!"}
+
+
+async def get_code(code: str = Query(..., description="OAuth2 authorization code")):
+    return code
+
+
+@router.get("/login/callback")
+async def callback(code: str = Depends(get_code)):
+    await exchange_code_for_token(code)
+
+    return {"message": "Authorization successful!"}
+
+
+async def exchange_code_for_token(code: str):
+    token_endpoint = "https://is.buk.cvut.cz/oauth/token"
+    client_credentials = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": code,
+        "redirect_uri": redirect_uri,
+        "grant_type": "authorization_code",
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(token_endpoint, data=client_credentials)
+        response_data = response.json()
+
+    # Save the token to a file
+    with open("token.txt", "w") as token_file:
+        token_file.write(response_data.get("access_token", ""))
+
+    return response_data.get("access_token", "")
--- app/app/main.py
+++ app/app/main.py
@@ -4,13 +4,16 @@ In other words it is an entry point of the application.
 """
 import uvicorn
 from fastapi import FastAPI
+from api import buk_is_auth
 
-app = FastAPI
+app = FastAPI()
+
+app.include_router(buk_is_auth.router)
 
 
 def main():
     uvicorn.run(app, host="10.0.52.106", port=8000,
-                ssl_keyfile="key.pem", ssl_certfile="cert.pem")
+                ssl_keyfile="certification/key.pem", ssl_certfile="certification/cert.pem")
 
 
 if __name__ == "__main__":
