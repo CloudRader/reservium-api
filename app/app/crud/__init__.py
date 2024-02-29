56910d0544ccd11ea97f84e5f732f92f52910be9
Artem Kuznetsov <artyom.20century@gmail.com>
2024-02-29 18:59:31 +0100

    ✅[Init] "Crud a service base"

---
+++ app/app/crud/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package for CRUD repositories for each domain type, used to handle
+operations over database.
+"""
+from .crud_base import AbstractCRUDBase, CRUDBase
+
+__all__ = ["AbstractCRUDBase", "CRUDBase"]
---
+++ app/app/services/__init__.py
@@ -0,0 +1,6 @@
+"""
+Package for Services.
+"""
+from .service_base import AbstractCRUDService, CrudServiceBase
+
+__all__ = ["AbstractCRUDService", "CrudServiceBase"]
