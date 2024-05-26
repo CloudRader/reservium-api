78cc6f470f17236d243c01fb06a415470a242639
Artem Kuznetsov <artyom.20century@gmail.com>
2024-05-26 22:03:27 +0200

    🛠️[Fixed] "Test"

--- app/tests/test_services/test_event.py
+++ app/tests/test_services/test_event.py
@@ -38,4 +38,4 @@ def test_post_event(create_user_manager_stud, service_event,
     )
     assert event is not None
     assert event["message"] == \
-           "You can't reserve in this gap!"
+           "You can't make reservations at night!"
