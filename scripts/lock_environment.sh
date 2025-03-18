65416e7448129573a37598fb2e7a5c0f25e48350
Artem Kuznetsov <artyom.20century@gmail.com>
2025-03-18 19:53:58 +0100

    🛠️[Fix] "Env, libries, conda-lock and scripts"

--- scripts/lock_environment.sh
+++ scripts/lock_environment.sh
@@ -4,4 +4,4 @@ set -e # stop script execution after failure of any command
 env_file_path="environment-dev.yml"
 lock_file_path="conda-lock.yml"
 
-conda-lock -f $env_file_path --lockfile $lock_file_path --platform=linux-aarch64
+conda-lock -f $env_file_path --lockfile $lock_file_path --platform=linux-aarch64 --platform=osx-arm64
